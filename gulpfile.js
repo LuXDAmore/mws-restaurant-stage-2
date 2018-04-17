367b1a14cc399ae28eef99343e506e9b82caaf77
Luca Iaconelli <lucabelli@msn.com>
2018-04-17 19:16:32 +0200

    Fix: Gulps task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-// TODO: muovere Maps loading forse, onload tutto, async defer tutto.
+// TODO: muovere Maps loading forse, onload tutto, async defer tutto, più load immagini per desktop?
 
 var gulp = require( 'gulp' )
 	, sequence = require( 'gulp-sequence' ).use( gulp )
